a1de51cd1fe59426493cffe109bb545f621fb87a
BoilUbuntu <free22858@live.com>
2020-06-20 13:18:00 +0800

    update

--- rendu/base/tests/Atomic_unittest.cc
+++ rendu/base/tests/Atomic_unittest.cc
@@ -1,11 +1,16 @@
-#include "rendu/base/includes/Atomic.h"
 #include <assert.h>
+#include "../includes/Atomic.h"
 
 int main()
 {
     {
         rendu::AtomicInt64 a0;
-        assert(a0.get() == 0);
+        long n = a0.get();
+        if (n == 1)
+        {
+        }
+
+        assert(a0.get() == 1);
         assert(a0.getAndAdd(1) == 0);
         assert(a0.get() == 1);
         assert(a0.addAndGet(2) == 3);
@@ -33,5 +38,18 @@ int main()
         assert(a1.addAndGet(-3) == 2);
         assert(a1.getAndSet(100) == 2);
         assert(a1.get() == 100);
+    }
+
+    {
+        rendu::AtomicBoolean a1;
+        bool b = a1.get();
+        if (b)
+        {
+            /* code */
+        }
+
+        assert(a1.get() == false);
+        assert(a1.getAndSet(true) == false);
+        assert(a1.get() == true);
     }
 }
